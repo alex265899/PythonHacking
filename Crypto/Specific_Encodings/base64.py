c0b6df5f8f83a50f45d0c0ec2450b28a00ae5148
Alexander Chapman <ouralexander@gmail.com>
2016-10-22 13:31:51 -0400

    Changed the base64 file to support most/all base encryption types.

--- Crypto/Specific_Encodings/base64.py
+++ Crypto/Specific_Encodings/base64.py
@@ -1,23 +1,67 @@
 #!usr/bin/env python27
 import base64
 
+#============================================
+# Base64
+# Encoding string to base64
 def base64_encode(x):
     try:
         return base64.b64encode(x)
     except BaseException:
         return "Encryption Not Possible"
 
+# Decoding string from base64
 def base64_decode(x):
     try:
         return base64.b64_decode(x)
     except BaseException:
         return "Decryption not possible"
+# =============================================
+# Base32
+# Encoding string to base32
+def base32_encode(x):
+    try:
+        return base64.b32encode(x)
+    except BaseException:
+        return "Encryption not possible"
 
-print "Base64 Functions..."
+# Decoding string from base32
+def base32_decode(x):
+    try:
+        return base64.b32decode(x)
+    except BaseException:
+        return "Decoding not possible"
+# ============================================
+# Base16
+# Encoding string to base16
+def base16_encode(x):
+    try:
+        return base64.b16encode(x):
+    except BaseException:
+        return "Encryption not possible"
+
+# Decoding from base16
+def base16_decode(x):
+    try:
+        return base64.b16decode(x)
+    except BaseException:
+        return "Decryption not possible"
+
+print "Base64/32/16 Encoding/Decoding"
 usrInput = raw_input("String >>> ")
 
-while usrInput != "":
-    print "Encoded: {}".format(base64_encode(usrInput))
-    print "Decoded: {}".format(base64_decode(usrInput))
-    usrInput = raw_input("String >>> ")
-    
+
+# setting newline variable
+linebreak = "====================================================="
+print linebreak
+
+print "Base64 Results:"
+print "     Encryption: {}".format(base64_encode(usrInput))
+print "     Decryption: {}".format(base64_decode(usrInput))
+print linebreak
+print "Base32 Results:"
+print "     Encryption: {}".format(base32_encode(usrInput))
+print "     Decryption: {}".format(base32_decode(usrInput))
+print linebreak
+print "Base16 Results"
+print "     "
