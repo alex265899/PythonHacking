286c4f0a44a5611b3aecb559f789daa0273a9384
Alexander Chapman <ouralexander@gmail.com>
2016-11-30 21:05:59 -0500

    Halfway ported base64.py; in fact, i rewrote the whole script! Hexadecimal_encryption.py is ported to python 3, still need to run some tests.

--- Crypto/Specific_Encodings/hexadecimal_encryption.py
+++ Crypto/Specific_Encodings/hexadecimal_encryption.py
@@ -1,4 +1,4 @@
-# /usr/bin/env python27
+# /usr/bin/env python3.4
 
 # defining the function to turn a number to hexadecimal. This also includes error handling.
 def to_hex(x):
@@ -7,20 +7,18 @@ def to_hex(x):
         return x
     except ValueError:
         return "Integer not given!"
-    except BaseException:
+    except:
         return "An error occurred. Try again!"
 
 
-
-print "This is for encrypting a number to hexadecimal. Please only enter a number!"
-
+print("This is for encrypting a number to hexadecimal. Please only enter a number!")
 
 # getting the first number from the user to turn to hex. This is also used to start the loop.
-usr_input = raw_input("Number >>> ")
+usr_input = input("Number >>> ")
 
 # turning number to hex and printing it out. Starting loop after this.
-print to_hex(usr_input)
+print(to_hex(usr_input))
 
 while usr_input != "":
-    usr_input = raw_input("Number >>> ")
-    print to_hex(usr_input)
+    usr_input = input("Number >>> ")
+    print(to_hex(usr_input))
