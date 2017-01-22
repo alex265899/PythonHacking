68893b4e21674cc9bce583d71fd1f4779113cd71
alex265899 <ouralexander@gmail.com>
2017-01-22 18:05:16 -0500

    Did a simple edit of get_requests.py and post_requests.py to port it to python 3.5; just changed raw_input() to input(), and print "" to print("")

--- WebsiteTools/Requests/get_requests.py
+++ WebsiteTools/Requests/get_requests.py
@@ -1,17 +1,16 @@
-# get_requests.py
-# usr/bin/python27 Python 2.7
+# usr/bin/python27 Python3.5
 
 # importing request library
 import requests
 
 # setting the req variable
-req = requests.get(raw_input("Complete URL: "))
+req = requests.get(input("Complete URL: "))
 
 # printing out the response code
-print "Response Code: " + str(req.status_code) + '\n'
+print("Response Code: " + str(req.status_code) + '\n')
 # printing out headers
-print "Headers: "
-print req.headers
-print ''
+print("Headers: ")
+print(req.headers)
+print('')
 # printing out html
-print "Response:\n" + req.text
+print("Response:\n" + req.text)
